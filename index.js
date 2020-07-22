01aeb4684ed3e89e8b4f36dcb65389318ca9f655
Nwakasi Stephen Ifeanyi <nwakasistephen@gmail.com>
2020-07-22 08:34:05 +0100

    feature(validation):validate keys - implement a method to validate the public key - implement a method to validate the private key - implement a method to validate the webdev key - implement the `icrc32` using the nodejs implementation of the crc32 checksum

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const Cryptobox = require('./lib/Cryptobox');
 
 const cryptobox = new Cryptobox();
+
+const result = cryptobox.icrc32(
+  'Time is the only thing that is never going to chnage its nature, after God'
+);
+console.log(result);
