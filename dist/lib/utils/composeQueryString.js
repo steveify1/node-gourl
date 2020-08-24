39c2c5e3991b92eaa2054d17d3b92b4381165a40
Nwakasi Stephen Ifeanyi <nwakasistephen@gmail.com>
2020-08-24 06:10:22 +0100

    feature:(payment):check payment status
    - start the implementation of a `checkPaymentStatus` method that
    allows the user to send a request the gourl api to check the status
    of a payment. The method takes an `options` parameter that include a
    set of data associated with the payment whose status is to be confirmed.
    
    [Starts #26]

---
+++ dist/lib/utils/composeQueryString.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * Converts an object into a valid URI query string
+ * @param { object } data - An object whose key-value pairs
+ * will be converted into a URI string
+ *
+ * @return { string } A HTTP query string starting with the
+ * question mark (?).
+ */
+exports.default = (data = {}) => {
+    let result = '?';
+    for (let key in data) {
+        let { value } = Object.getOwnPropertyDescriptor(data, key);
+        value = `${value}`.split(' ').join('+');
+        result += `${key}=${encodeURIComponent(value)}&`;
+    }
+    // remove trailing `&`
+    result = result.slice(0, result.length - 1);
+    return result;
+};
---
+++ lib/utils/formatExpiryPeriod.ts
@@ -0,0 +1,24 @@
+export default (period: string): string => {
+  period = period.replace(' ', '').toUpperCase().trim();
+  if (period.substring(-1) === 'S') {
+    period = period.substring(0, -1);
+  }
+
+  const arr: string[] = [];
+  for (let i = 1; i <= 90; i++) {
+    arr.push(`${i}MINUTE`);
+    arr.push(`${i}HOUR`);
+    arr.push(`${i}DAY`);
+    arr.push(`${i}WEEK`);
+    arr.push(`${i}MONTH`);
+  }
+
+  if (period !== 'NOEXPIRY' && !arr.includes(period)) {
+    throw new Error(`Invalid Cryptobox Period - ${period}`);
+  }
+
+  return period.replace(
+    /(minute|hour|day|week|month)/i,
+    (match) => ` ${match}`
+  );
+};
